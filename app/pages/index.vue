a2d31b2ece706d973353f45581de4ce6d96ce4d6
rzkir <rr8027896@gmail.com>
2026-02-13 15:17:25 +0700

    [update] setup

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-red-500">
+    <h1>Hallo</h1>
+    <NuxtLink class="text-blue-500" to="/about">About</NuxtLink>
+  </div>
+</template>
+
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui'
+  }
+})
